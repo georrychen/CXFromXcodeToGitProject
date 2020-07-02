392a6444ce17eac98c7fa1eb050c7ee05ee635bb
chenxu <2235037295@qq.com>
2020-07-02 09:28:57 +0800

    fix

--- CXFromXcodeToGitProject/ViewController.m
+++ CXFromXcodeToGitProject/ViewController.m
@@ -21,16 +21,19 @@
     
     /*
      
-     在 Xcode 自带 Git 的使用
+     在 Xcode 自带 Git 的使用1
      https://www.cnblogs.com/Mr-zyh/p/7682789.html
      
-     1. 新建工程，选中使用git，创建本地仓库
+     1.新建工程，选中使用git，创建本地仓库
      
-     2. 在偏好设置中添加 Git 账号（ 登录方式：邮箱+token ）
+     2.在偏好设置中添加 Git 账号（ 登录方式：邮箱+token ）
          获取 personal access token 的方法：在你的 Git 账号设置中可以获取到
          https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token
      
-     3.
+     3.创建Git远程仓库，xcode会自动帮你在Git上创建
+        右键，create "xx" remote
+     
+     4.创建好以后，就可以在 Source Control 中进行 pull commit 等操作了
      
   
      */
