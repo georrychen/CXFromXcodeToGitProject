6d1094d14175865612a47ef20f5b8597514923fb
chenxu <2235037295@qq.com>
2020-07-02 09:20:50 +0800

    init

--- CXFromXcodeToGitProject/ViewController.m
+++ CXFromXcodeToGitProject/ViewController.m
@@ -16,7 +16,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+   
+    
+    
+    /*
+     
+     在 Xcode 自带 Git 的使用
+     https://www.cnblogs.com/Mr-zyh/p/7682789.html
+     
+     1. 新建工程，选中使用git，创建本地仓库
+     
+     2. 在偏好设置中添加 Git 账号（ 登录方式：邮箱+token ）
+         获取 personal access token 的方法：在你的 Git 账号设置中可以获取到
+         https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token
+     
+     3.
+     
+  
+     */
+    
 }
 
 
